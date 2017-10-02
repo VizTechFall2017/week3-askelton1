f5fa7642282b887df6ec18be6c80b296e848402e
askelton1 <askelton1@github.com>
2017-10-02 17:05:16 -0400

    ps01

--- PS01/index.js
+++ PS01/index.js
@@ -1,3 +1,65 @@
 var svg = d3.select('svg');
 
-/* Your code goes here */
+/*var array = [ "milk", "eggs", "bread", "butter"];
+
+console.log(array[0]);
+
+var student = {"name":"abby", "job": "student", "age": "24"};
+
+console.log(student.age);
+
+var classRoom = [
+    {"name":"abby", "job": "student", "age": "24"},
+    {"name":"abby", "job": "student", "age": "24"},
+    {"name":"abby", "job": "student", "age": "24"}
+];
+
+console.log(classRoom);
+
+console.log( classRoom[1]);*/
+
+myCircle = {"cx": 100, "cy": 200, 'r': 20, 'color':'pink'} ;
+
+myArray = [{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}];
+
+for (i=0; i < 25; i++) {
+
+    console.log("circle" +i);
+
+    myArray[i].name = "circle" + i;
+
+    myArray[i].cx = Math.random(i) * 500;
+    myArray[i].cy = Math.random(i) * 500;
+    myArray[i].r = Math.random(i) * 20;
+}
+svg.selectAll('circle')
+    .data(myArray)
+    .enter()
+    .append('circle')
+    .attr('cx', function(d){
+        console.log(d.cx);
+        return d.cx;
+    })
+    .attr('cy', function(d) {
+        return d.cy;
+    })
+    .attr('r', function(d){
+        return d.r;
+    })
+    .attr('fill', function(d) {
+        return d.color;
+    });
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+
+
+
